b80cf9e32a517ee72c8809ab34e0872437e88e92
Nico Wallmeier <Nico@Wallmeier.net>
2019-11-14 20:11:07 +0100

    Refactored class HSDWifi + setting configured hostname

--- HSDWifi.cpp
+++ HSDWifi.cpp
@@ -1,124 +1,92 @@
 #include "HSDWifi.hpp"
-#include <ESP8266WiFi.h>
 
 #define SOFT_AP_SSID  (F("StatusDisplay"))
 #define SOFT_AP_PSK   (F("statusdisplay"))
 
-HSDWifi::HSDWifi(const HSDConfig& config)
-:
-m_config(config),
-m_connectFailure(false),
-m_maxConnectRetries(100),
-m_numConnectRetriesDone(0),
-m_retryDelay(500),
-m_millisLastConnectTry(0),
-m_accessPointActive(false),
-m_lastConnectStatus(false),
-m_wasConnected(false)
-{
-  
+HSDWifi::HSDWifi(const HSDConfig& config) :
+    m_config(config),
+    m_connectFailure(false),
+    m_maxConnectRetries(100),
+    m_numConnectRetriesDone(0),
+    m_retryDelay(500),
+    m_millisLastConnectTry(0),
+    m_accessPointActive(false),
+    m_lastConnectStatus(false),
+    m_wasConnected(false)
+{  
 }
 
-void HSDWifi::begin()
-{
-  // nothing to do right now
-  WiFi.persistent(false);
+// ---------------------------------------------------------------------------------------------------------------------
+
+void HSDWifi::begin() {
+    WiFi.persistent(false);
+    WiFi.hostname(m_config.getHost());
 }
 
-void HSDWifi::handleConnection()
-{
-  bool isConnected = connected();
+// ---------------------------------------------------------------------------------------------------------------------
 
-  if(isConnected != m_lastConnectStatus)
-  {
-    if(isConnected)
-    {
-      Serial.print(F("WiFi connected with IP "));
-      Serial.print(WiFi.localIP());
-      Serial.println(F("."));
-
-      m_wasConnected = true;
-      
-      m_numConnectRetriesDone = 0;
-    }
-    else
-    {
-      Serial.println(F("WiFi connection lost."));
-    }
-    
-    m_lastConnectStatus = isConnected;
-  }
-  
-  if(!isConnected && !m_accessPointActive)
-  {  
-    if(m_connectFailure)
-    {
-      startAccessPoint();
-    }
-    else
-    {
-      if( (millis() - m_millisLastConnectTry) >= m_retryDelay)
-      {
-        m_millisLastConnectTry = millis(); 
-      
-        if(m_numConnectRetriesDone == 0)
-        {
-          Serial.print(F("Starting Wifi connection to "));
-          Serial.print(m_config.getWifiSSID());
-          Serial.println(F("..."));
-      
-          WiFi.mode(WIFI_STA);
-          WiFi.begin(m_config.getWifiSSID(), m_config.getWifiPSK());
-      
-          m_numConnectRetriesDone++;
+void HSDWifi::handleConnection() {
+    bool isConnected(connected());
+    if (isConnected != m_lastConnectStatus) {
+        if (isConnected) {
+            Serial.print(F("WiFi connected with IP "));
+            Serial.print(WiFi.localIP());
+            Serial.println(F("."));
+            m_wasConnected = true;
+            m_numConnectRetriesDone = 0;
+        } else {
+            Serial.println(F("WiFi connection lost."));
         }
-        else if(m_numConnectRetriesDone < m_maxConnectRetries)
-        {
-          m_numConnectRetriesDone++;
-        }
-        else
-        {
-          Serial.println(F("Failed to connect WiFi."));
-
-          // if successfully connected before reboot otherwise start access point
-          if(m_wasConnected)
-          {
-            ESP.restart();
-          }
-          else
-          {
-            m_connectFailure = true;
-          }
-        }   
-      }   
+        m_lastConnectStatus = isConnected;
     }
-  }
+  
+    if (!isConnected && !m_accessPointActive) {  
+        if (m_connectFailure) {
+            startAccessPoint();
+        } else {
+            if ((millis() - m_millisLastConnectTry) >= m_retryDelay) {
+                m_millisLastConnectTry = millis(); 
+                if (m_numConnectRetriesDone == 0) {
+                    Serial.print(F("Starting Wifi connection to "));
+                    Serial.print(m_config.getWifiSSID());
+                    Serial.println(F("..."));
+      
+                    WiFi.mode(WIFI_STA);
+                    WiFi.begin(m_config.getWifiSSID(), m_config.getWifiPSK());
+      
+                    m_numConnectRetriesDone++;
+                } else if (m_numConnectRetriesDone < m_maxConnectRetries) {
+                    m_numConnectRetriesDone++;
+                } else {
+                    Serial.println(F("Failed to connect WiFi."));
+
+                    // if successfully connected before reboot otherwise start access point
+                    if (m_wasConnected) {
+                        ESP.restart();
+                    } else {
+                        m_connectFailure = true;
+                    }
+                }   
+            }   
+        }
+    }
 }
 
-bool HSDWifi::connected()
-{
-  return (WiFi.status() == WL_CONNECTED);
+// ---------------------------------------------------------------------------------------------------------------------
+
+void HSDWifi::startAccessPoint() {
+    Serial.println(F(""));
+    Serial.println(F("Starting access point."));
+
+    WiFi.mode(WIFI_AP);
+
+    if (WiFi.softAP(String(SOFT_AP_SSID).c_str(), String(SOFT_AP_PSK).c_str())) {
+        IPAddress ip = WiFi.softAPIP();
+        Serial.print(F("AccessPoint SSID is ")); Serial.println(SOFT_AP_SSID); 
+        Serial.print(F("IP: ")); Serial.println(ip);
+
+        m_accessPointActive = true;
+    } else {
+        Serial.println(F("Error starting access point."));
+    }
 }
-
-void HSDWifi::startAccessPoint()
-{
-  Serial.println(F(""));
-  Serial.println(F("Starting access point."));
-
-  WiFi.mode(WIFI_AP);
-
-  if(WiFi.softAP(String(SOFT_AP_SSID).c_str(), String(SOFT_AP_PSK).c_str()))
-  {
-    IPAddress ip = WiFi.softAPIP();
- 
-    Serial.print(F("AccessPoint SSID is ")); Serial.println(SOFT_AP_SSID); 
-    Serial.print(F("IP: ")); Serial.println(ip);
-
-    m_accessPointActive = true;
-  }
-  else
-  {
-    Serial.println(F("Error starting access point."));
-  }
-}
-
--- HSDWifi.hpp
+++ HSDWifi.hpp
@@ -1,29 +1,30 @@
-#pragma once
+#ifndef HSDWIFI_H
+#define HSDWIFI_H
+
+#include <ESP8266WiFi.h>
 
 #include "HSDConfig.hpp"
 
-class HSDWifi
-{
+class HSDWifi {
 public:
+    HSDWifi(const HSDConfig& config);
 
-  HSDWifi(const HSDConfig& config);
-
-  void begin();
-  void handleConnection();
-  bool connected();
+    void        begin();
+    inline bool connected() const { return (WiFi.status() == WL_CONNECTED); }
+    void        handleConnection();
 
 private:
+    void startAccessPoint();
 
-  void startAccessPoint();
-
-  const HSDConfig& m_config;
-  bool m_connectFailure;
-  int m_maxConnectRetries;
-  int m_numConnectRetriesDone;
-  int m_retryDelay;
-  unsigned long m_millisLastConnectTry;
-  bool m_accessPointActive;
-  bool m_lastConnectStatus;
-  bool m_wasConnected;
+    bool             m_accessPointActive;
+    const HSDConfig& m_config;
+    bool             m_connectFailure;
+    bool             m_lastConnectStatus;
+    int              m_maxConnectRetries;
+    unsigned long    m_millisLastConnectTry;
+    int              m_numConnectRetriesDone;
+    int              m_retryDelay;
+    bool             m_wasConnected;
 };
 
+#endif // HSDWIFI_H
