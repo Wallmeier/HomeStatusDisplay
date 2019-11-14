6df80d2b7b777688d45d89a724ba3490efc7c718
Nico Wallmeier <Nico@Wallmeier.net>
2019-11-14 19:03:19 +0100

    Refactored class HSDHtmlHelper

--- HSDHtmlHelper.hpp
+++ HSDHtmlHelper.hpp
@@ -1,39 +1,36 @@
-#pragma once
+#ifndef HSDHTMLHELPER_H
+#define HSDHTMLHELPER_H
 
 #include <Arduino.h>
-#include "HSDConfig.hpp"
 #include <ESP8266WebServer.h>
 
-class HSDHtmlHelper
-{
+#include "HSDConfig.hpp"
 
+class HSDHtmlHelper {
 public:
+    String getHeader(const char* title, const char* host, const char* version) const;
+    String getFooter() const;
 
-  HSDHtmlHelper();
+    String getColorMappingTableHeader() const;
+    String getColorMappingTableEntry(int entryNum, const HSDConfig::ColorMapping* mapping, const String& colorString) const;
+    String getColorMappingTableFooter() const;
+    String getColorMappingTableAddEntryForm(int newEntryNum, bool isFull) const;
 
-  String getHeader(const char* title, const char* host, const char* version) const;
-  String getFooter() const;
+    String getDeviceMappingTableHeader() const;
+    String getDeviceMappingTableEntry(int entryNum, const HSDConfig::DeviceMapping* mapping) const;
+    String getDeviceMappingTableFooter() const;
+    String getDeviceMappingTableAddEntryForm(int newEntryNum, bool isFull) const;
 
-  String getColorMappingTableHeader() const;
-  String getColorMappingTableEntry(int entryNum, const HSDConfig::ColorMapping* mapping, String colorString) const;
-  String getColorMappingTableFooter() const;
-  String getColorMappingTableAddEntryForm(int newEntryNum, bool isFull) const;
+    String getDeleteForm() const;
+    String getSaveForm() const;
 
-  String getDeviceMappingTableHeader() const;
-  String getDeviceMappingTableEntry(int entryNum, const HSDConfig::DeviceMapping* mapping) const;
-  String getDeviceMappingTableFooter() const;
-  String getDeviceMappingTableAddEntryForm(int newEntryNum, bool isFull) const;
-
-  String getDeleteForm() const;
-  String getSaveForm() const;
-
-  String minutes2Uptime(unsigned long minutes) const;
-  String ip2String(IPAddress ip) const;
-  String behavior2String(HSDConfig::Behavior behavior) const;
+    String minutes2Uptime(unsigned long minutes) const;
+    String ip2String(IPAddress ip) const;
+    String behavior2String(HSDConfig::Behavior behavior) const;
     
 private:
-
-  String getBehaviorOptions(HSDConfig::Behavior selectedBehavior) const;
-  String getColorOptions() const;
+    String getBehaviorOptions(HSDConfig::Behavior selectedBehavior) const;
+    String getColorOptions() const;
 };
 
+#endif // HSDHTMLHELPER_H
